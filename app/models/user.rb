4f8a285bc6d5b710d278adb568bb9b92a945b7a3
kengo922 <kengo.matsuno.922@gmail.com>
2020-06-11 06:39:45 +0000

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,14 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+
 end
 
 #バリデーションの正規表現の付け方（下に記述）
